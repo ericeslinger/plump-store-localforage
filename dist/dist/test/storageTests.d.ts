f7add69e4de3d2aa05de8f8bda3a63c5ec7406f3
Eric Eslinger <eric.eslinger@gmail.com>
2017-05-31 11:29:48 -0700

    package for release

--- dist/dist/test/storageTests.d.ts
+++
@@ -1,9 +0,0 @@
-declare global  {
-    namespace Chai {
-        interface Assertion {
-            nested: Assertion;
-        }
-    }
-}
-export declare function testSuite(context: any, storeOpts: any): void;
-export {};
