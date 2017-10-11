00846ed8789d0e3c9aa44f52c399a6c69e9e3244
Eric Eslinger <eric.eslinger@gmail.com>
2017-10-11 16:10:16 -0700

    adding tests for date storage

---
+++ dist/dist/test/testType.d.ts
@@ -0,0 +1,33 @@
+import { ModelSchema, RelationshipSchema, Model, ModelData, RelationshipItem } from 'plump';
+export declare const ChildrenSchema: RelationshipSchema;
+export declare const ValenceChildrenSchema: RelationshipSchema;
+export declare const QueryChildrenSchema: RelationshipSchema;
+export declare const TestSchema: ModelSchema;
+export interface PermRelationshipItem extends RelationshipItem {
+    meta: {
+        perm: number;
+    };
+}
+export interface TestData extends ModelData {
+    type: 'tests';
+    id: number;
+    attributes?: {
+        id: number;
+        name: string;
+        otherName: string;
+        extended: {
+            [key: string]: any;
+        };
+    };
+    relationships?: {
+        children: RelationshipItem[];
+        parents: RelationshipItem[];
+        valenceChildren: PermRelationshipItem[];
+        valenceParents: PermRelationshipItem[];
+        queryChildren: PermRelationshipItem[];
+        queryParents: PermRelationshipItem[];
+    };
+}
+export declare class TestType extends Model<TestData> {
+    static type: string;
+}
